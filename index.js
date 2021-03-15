569bdc358cc516db5c0383de55b898e974e6ad15
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-15 18:22:50 -0500

    commit tree 29c05f3eba24f3d54d7677b4d49f1bbcf73549c9

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+let dodger = document.getElementById("dodger");
+document.addEventListener("keydown", function(e) {
+    console.log(e.key);
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+
+    }
+})
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 10}px`;
+    }
+}
+function moveDodgerRight() {
+    let rightNumbers = dodger.style.left.replace("px", "");
+    let right = parseInt(rightNumbers, 10);
+
+    if (right > 0) {
+        dodger.style.left = `${right + 10}px`;
+    }
+}
